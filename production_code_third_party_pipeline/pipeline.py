617cba3ac28736507d05a1352d6957f3815d0fff
Diogo Soares Ferreira <diogo.soares@talkdesk.com>
2020-11-15 18:38:26 +0000

    Add third assignment

---
+++ production_code_third_party_pipeline/pipeline.py
@@ -0,0 +1,32 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+import preprocessors as pp
+import config
+
+
+titanic_pipe = Pipeline(
+    [
+    	('extract_first_letter',
+    		pp.ExtractFirstLetter(variables=config.CABIN)),
+
+    	('missing_indicator',
+    		pp.MissingIndicator(variables=config.NUMERICAL_VARS)),
+
+    	('numerical_imputer',
+    		pp.NumericalImputer(variables=config.NUMERICAL_VARS)),
+
+    	('categorical_imputer',
+    		pp.CategoricalImputer(variables=config.CATEGORICAL_VARS)),
+
+    	('rare_label_encoder',
+    		pp.RareLabelCategoricalEncoder(variables=config.CATEGORICAL_VARS)),
+
+    	('categorical_encoder',
+    		pp.CategoricalEncoder(variables=config.CATEGORICAL_VARS)),
+
+    	('scaler', StandardScaler()),
+        ('Linear_model', LogisticRegression(C=0.0005, random_state=0))
+    ]
+)
